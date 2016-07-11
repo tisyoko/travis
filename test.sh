aa374c98ed69956a257948080259a369a49bbcb7
tisyoko <tisyoko@tisyoko.local>
2016-07-12 02:18:02 +0900

    update8

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 hoge=$1
-fuge=$2
+fuga=$2
 
 
 echo ${hoge}
 echo ${fuga}
 echo -----
 
-if  ${hoge} -eq 100 ; then
+if  ${hoge} = 100 ; then
   echo "ok"
 fi
 
-if  ${fuge} = "hoge" ; then
+if  ${fuga} = "hoge" ; then
   echo "ok2"
 fi
